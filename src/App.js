3640a54609fa0c05b333c39b108bdef01a676ff6
gradooid <graduses9@gmail.com>
2020-01-25 20:59:08 +0200

    Got response from OpenWeatherAPI and rendered some results

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+// import cities from './city.list.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      city: '',
+      API_KEY: '10fdb9377ecec456103c045bc8c7a376',
+      forecast: []
+    };
+
+    this.showForecast = this.showForecast.bind(this);
+  }
+
+  showForecast() {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${this.state.city}&APPID=${this.state.API_KEY}`
+      )
+      .then(res => this.setState({ forecast: res.data.list }));
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={e => this.setState({ city: e.target.value })}
+          value={this.state.city}
+        />
+        <button onClick={() => this.showForecast()}>Show forecast</button>
+        {this.state.forecast.map(day => (
+          <div>
+            <h4>{day.dt_txt}</h4>
+            <p>{day.clouds.all}</p>
+            <p>{day.main.feels_like}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-
-export default App;
