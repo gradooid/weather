f5ddbb601de483661cdb48d7dde6bb836d01e8dd
gradooid <graduses9@gmail.com>
2020-02-15 19:11:06 +0200

    Updates

---
+++ precache-manifest.825f4b9520aed7696f21314c90ae30ef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b436b23721fe374bdfa6477ea573051",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "7f44a7b9b51ce7f2f3a5",
+    "url": "/weather/static/css/main.a2e393bc.chunk.css"
+  },
+  {
+    "revision": "4cf6e00cb558ec038700",
+    "url": "/weather/static/js/2.ea849c5f.chunk.js"
+  },
+  {
+    "revision": "e2fc3609d47d90b8cc004bfe15ddf15e",
+    "url": "/weather/static/js/2.ea849c5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f44a7b9b51ce7f2f3a5",
+    "url": "/weather/static/js/main.eb37780c.chunk.js"
+  },
+  {
+    "revision": "c99e76bcf2bdae1a8b8a",
+    "url": "/weather/static/js/runtime-main.953fba7f.js"
+  }
+]);
